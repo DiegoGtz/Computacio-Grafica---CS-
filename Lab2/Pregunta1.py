20711ffe16a9b0b7ef67a5acdae06fd581033253
DARTZ-2\DartzPC-2 <dartz160@gmail.com>
2020-05-05 15:51:58 -0500

    3

--- Lab2/Pregunta1.py
+++ Lab2/Pregunta1.py
@@ -19,9 +19,27 @@ def Histograma(imagen):
 	plt.show()
 	cv2.destroyAllWindows()	
 
+def  Contrast_Stretching(width, height ,a , b ):
+	_w = width
+	_h = height
+
+
+
+	print (_w +1 ,_h, a,b )
+
+
 
 if __name__ == "__main__":
     
 	#Obtenemos el Histograma de la imagen
 	imagen = 'contr2.jpg'
 	Histograma(imagen)
+
+	#Stretching 
+	a,b = 0, 255
+
+	img  = cv2.imread(imagen,0)
+	width, height = img.shape[:2]
+
+
+	Contrast_Stretching(width,height,a,b)
