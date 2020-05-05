56ccb2fa61009be3a385afe1394aec1773272c32
DARTZ-2\DartzPC-2 <dartz160@gmail.com>
2020-05-05 15:40:42 -0500

    2

--- Lab2/Pregunta1.py
+++ Lab2/Pregunta1.py
@@ -4,16 +4,24 @@
 
 import cv2 
 import numpy as np
+from matplotlib import pyplot as plt
 #####################################
 
 
 
-def Histograma():
-img = cv2.imread('thresh1.png',0)
-hist = cv2.calcHist([img], [0], None, [256], [0, 256])
-plt.plot(hist, color='gray' )
+def Histograma(imagen):
+	img = cv2.imread(imagen ,0)
+	hist = cv2.calcHist([img], [0], None, [256], [0, 256])
+	plt.plot(hist, color='gray' )
 
-plt.xlabel('intensidad de iluminacion')
-plt.ylabel('cantidad de pixeles')
-plt.show()
-cv2.destroyAllWindows()	
+	plt.xlabel('intensidad de iluminacion')
+	plt.ylabel('cantidad de pixeles')
+	plt.show()
+	cv2.destroyAllWindows()	
+
+
+if __name__ == "__main__":
+    
+	#Obtenemos el Histograma de la imagen
+	imagen = 'contr2.jpg'
+	Histograma(imagen)
