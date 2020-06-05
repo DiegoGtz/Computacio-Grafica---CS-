56cde98b8390da1597110e693e7a77fb32b12c6d
DARTZ-2\DartzPC-2 <dartz160@gmail.com>
2020-06-05 09:42:49 -0500

    lab 7

--- Examen Grafica/Examen/apps/operadores/views.py
+++ Examen Grafica/Examen/apps/operadores/views.py
@@ -308,6 +308,125 @@ class Algoritmos ():
 		cv2.imwrite(salidaImg, img_out)
 
 		return salidaImg1'''
+
+	def multiplication(imagen1,c):
+		img1 = cv2.imread(imagen1)
+
+		img1 = img1.astype(int)
+
+		alto1,ancho1 = img1.shape[:2]
+		
+		#print(alto1,' ',ancho1)
+		
+		for i in range(alto1):
+			for j in range(ancho1):
+
+				img1[i][j][0] = img1[i][j][0] * c
+				img1[i][j][1] = img1[i][j][1] * c
+				img1[i][j][2] = img1[i][j][2] * c
+
+		salidaImg = "static/MultiplicacionOUT" + imagen1
+		cv2.imwrite(salidaImg,img1)
+		return salidaImg
+	def blending(imagen1,imagen2,x):
+		img1 = cv2.imread(imagen1)
+		img2 = cv2.imread(imagen2)
+		img1 = img1.astype(float)
+		img2 = img2.astype(float)
+
+		alto1,ancho1 = img1.shape[:2]
+		alto2,ancho2 = img2.shape[:2]
+		
+		alto = 0
+		ancho = 0
+
+		if alto1<alto2:
+			alto=alto1
+		else:
+			alto=alto2
+		if ancho1<ancho2:
+			ancho=ancho1
+		else:
+			ancho=ancho2
+
+		for i in range(alto):
+			for j in range(ancho):
+				img1[i][j][0] = (x * img1[i][j][0]/2)+((1-x)*img2[i][j][0]/2)
+				img1[i][j][1] = (x * img1[i][j][1]/2)+((1-x)*img2[i][j][1]/2)
+				img1[i][j][2] = (x * img1[i][j][2]/2)+((1-x)*img2[i][j][2]/2)
+
+		salidaImg = "static/Blending" + imagen1 + imagen2
+		cv2.imwrite(salidaImg,img1)
+		return salidaImg
+
+	def pixel_division(imagen1,imagen2):
+		print (imagen1,imagen2)
+		img1 = cv2.imread(imagen1,0)
+		img2 = cv2.imread(imagen2,0)
+		#img1 = img1.astype(int)
+		#img2 = img2.astype(int)
+
+		alto,ancho = img1.shape
+
+		for i in range(alto):
+			for j in range(ancho):
+				img1[i][j] = (img1[i][j]/img2[i][j])*30
+
+		newMin=0
+		newMax=255
+		mini=0
+		maxi=30
+		for i in range(alto):
+			for j in range(ancho):
+				img1[i][j] = (img1[i][j]-mini)*((newMax-newMin)/(maxi-mini))+newMin
+
+		img_out=img1
+		for i in range(alto):
+		    for j in range(ancho):
+		        #if (img[i][j]<130):
+		        if ( 50 < img1[i,j] and img1[i,j] < 215):
+		            img_out[i,j]=0
+		        else:
+		            img_out[i,j]=255
+
+
+		salidaImg = "static/Div" + imagen1 + imagen2
+		cv2.imwrite(salidaImg,img_out)
+		return salidaImg
+
+	'''def scaling(imagen):
+		img = cv2.imread(imagen,0)
+		img = img.astype(int)
+		alto,ancho = img.shape
+
+		newMin=0
+		newMax=255
+		mini=0
+		maxi=30
+
+		for i in range(alto):
+			for j in range(ancho):
+				img[i][j] = (img[i][j]-mini)*((newMax-newMin)/(maxi-mini))+newMin
+
+		salidaImg = "static/Sca" + imagen
+		cv2.imwrite(salidaImg,img)
+
+	def Thresholding(imagen):
+		img = cv2.imread(imagen,0)
+		alto,ancho = img.shape
+
+		img_out=img
+		for i in range(alto):
+		    for j in range(ancho):
+		        #if (img[i][j]<130):
+		        if ( 50 < img[i,j] and img[i,j] < 215):
+		            img_out[i,j]=0
+		        else:
+		            img_out[i,j]=255
+		imagen = 
+		cv2.imwrite('thress.jpg',img_out)'''
+
+
 class Operadores():
 	def inicio(request):
 		return render(request,'Home.html')
@@ -339,13 +458,20 @@ class Operadores():
 			return render(request,'Addition.html',{"labels":tipo})	
 		elif(tipo == "Subtraction"):
 			return render(request,'Subtraction.html',{"labels":tipo})	
+
+		elif(tipo == "Multiplicacion" ):
+			return render(request,'Multiplicacion.html',{"labels":tipo})
+		elif(tipo == "Blending" ):
+			return render(request,'Blending.html',{"labels":tipo})
+		elif(tipo == "Division" ):
+			return render(request,'Division.html',{"labels":tipo})
 	def ControladorOperador(request):
 
 		#id = request.POST['fase']
 		tipo = request.POST['Tipo']
 		myfile = request.FILES["file1"]
 		myfile2 = request.FILES["file2"]
-		print(myfile,myfile2)
+		#print(myfile,myfile2)
 		fs = FileSystemStorage()
 		fs2 = FileSystemStorage()
 		filename = fs.save(myfile.name, myfile)
@@ -353,7 +479,7 @@ class Operadores():
 		file_name = fs.url(filename)
 		file_name2 = fs2.url(filename2)
 
-		print(file_name,file_name2)
+		#print(file_name,file_name2)
 		if(tipo == "Thresholding"):
 			min1 = request.POST['min']
 			max2 = request.POST['max']			
@@ -427,7 +553,7 @@ class Operadores():
 
 
 		if (tipo == "Add"):
-
+			#return render(request,'Home.html')
 			resultado = Algoritmos.pixel_adition(file_name,file_name2)
 		
 			return render(request,'ResulTOperador.html',{"labels2":tipo,"image":"/"+resultado} )
@@ -436,7 +562,22 @@ class Operadores():
 			resultado = Algoritmos.pixel_sustraction(file_name,file_name2)
 			
 			return render(request,'ResulTOperador.html',{"labels2":tipo,"image":"/"+resultado} )
-			
+		if (tipo == "Multiplicacion"):
+			c = request.POST['c']
+			resultado = Algoritmos.multiplication(file_name,int(c))
+		
+			return render(request,'ResulTOperador.html',{"labels2":tipo,"image":"/"+resultado} )
+		if (tipo == "Blending"):
+			#return render(request,'Home.html')
+			x = request.POST['x']
+
+			resultado = Algoritmos.blending(file_name,file_name2,float(x))
+			return render(request,'ResulTOperador.html',{"labels2":tipo,"image":"/"+resultado} )
+		if (tipo == "Division"):
+			#return render(request,'Home.html')
+			resultado = Algoritmos.pixel_division(file_name,file_name2)
+		
+			return render(request,'ResulTOperador.html',{"labels2":tipo,"image":"/"+resultado} )
 		return render(request,'Home.html')
 
 
