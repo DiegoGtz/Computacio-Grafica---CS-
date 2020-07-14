5cd382d608466962efae901655aed588b58cecf4
DARTZ-2\DartzPC-2 <dartz160@gmail.com>
2020-07-13 23:54:49 -0500

    nuevo

--- Examen Grafica/Examen/apps/operadores/views.py
+++ Examen Grafica/Examen/apps/operadores/views.py
@@ -18,15 +18,12 @@ def inicio(request):
     return render(request,'Home.html')
 
 class Algoritmos ():
-
-
 	def Thresholding(img1,min,max):
 		imagen = img1
 		img  = cv2.imread(imagen)
 		alto, ancho = img.shape[:2]
 		matriz3= np.array(img)
 		matriz = []
-
 		for y in range(0, alto):
 			matriz.append([])
 			for x in range(0,ancho):
@@ -34,15 +31,13 @@ class Algoritmos ():
 				g = img.item(y, x, 1)
 				r = img.item(y, x, 2)
 				if (b > min and b <max)  and (g > min and g <max ) and (r > min and r <max):
-					matriz[y].append(0)#Blanco
+					matriz[y].append(255)#Blanco
 				else:
-					matriz[y].append(255)#Negro
+					matriz[y].append(0)#Negro
 		_toNP = np.array(matriz)
 		salidaImg = "static/Thresholding" + imagen
 		cv2.imwrite(salidaImg,_toNP)		
 		return salidaImg
-
-
 	def Outlier_Contrast_Stretching(img1, a,b, Low, High):
 		imagen = img1 
 		img  = cv2.imread(imagen,0)
@@ -277,7 +272,6 @@ class Algoritmos ():
 		salidaImg = "static/Adition.jpg"
 		cv2.imwrite(salidaImg,img1)
 		return salidaImg
-
 	def pixel_sustraction(imagen1,imagen2):
 		img1 = cv2.imread(imagen1,0)
 		img2 = cv2.imread(imagen2,0)
@@ -308,7 +302,6 @@ class Algoritmos ():
 		salidaImg1 = "static/prueba_salida.png" +imagen1 
 		cv2.imwrite(salidaImg1,img_out)
 		return salidaImg1
-
 	def multiplication(imagen1,c):
 		img1 = cv2.imread(imagen1)
 
@@ -356,7 +349,6 @@ class Algoritmos ():
 		salidaImg = "static/Blending" + imagen1 + imagen2
 		cv2.imwrite(salidaImg,img1)
 		return salidaImg
-
 	def pixel_division(imagen1,imagen2):
 
 		img1 = cv2.imread(imagen1,0)
@@ -393,7 +385,6 @@ class Algoritmos ():
 		salidaImg = "static/Div" + imagen1 + imagen2
 		cv2.imwrite(salidaImg,img_out)
 		return salidaImg
-
 	def Ope_Logicos(imagen1,imagen2,operador):
 		img1 = cv2.imread(imagen1,0)
 		img2 = cv2.imread(imagen2,0)
@@ -428,9 +419,98 @@ class Algoritmos ():
 		cv2.imwrite(salidaImg,img1)
 		return salidaImg
 
+#Funciones CamScam -------------------------------------------------------
+
+	def warPerspective(img,M,new_img):
+		img_out = np.zeros([new_img[1],new_img[0],3],dtype = np.uint32)
+		row,columns = (new_img[0],new_img[1])
+		A= np.array(M,dtype = np.float64)
+		B= np.array(M[:,2:],dtype = np.float64)
+		for u in range(0,row):
+			for v in range(0,columns): 
+				Y =  np.array([[u], [v],[1]], dtype=np.float64)
+				E = cv2.solve(M,Y)[1]
+				_x = (E[0,0])
+				_y = (E[1,0])
+				_z = (E[2,0])
+				img_out[v,u] = img[int(_y//_z),int(_x//_z)]
+		return np.uint8(img_out)
+	def get_M(X,M):
+		c = 0 
+		for i in range(3):
+			for j in range(3):
+				if (c !=8  ):
+					M[i][j] = X[c][0]
+					c = c+1
+		M[2][2]= 1
+		return M
+	def getPerspectiveTransform(src,dst):
+		M = np.zeros((3, 3), dtype=np.float64)
+		A = np.zeros((8, 8), dtype=np.float64)
+		B = np.zeros((8, 1), dtype=np.float64)
+		X = np.zeros((8, 1), dtype=np.float64)
+		for i in range(4):
+		  	A[i][0] = A[i+4][3] = src[i][0]
+		  	A[i][1] = A[i+4][4] = src[i][1]
+		  	A[i][2] = A[i+4][5] = 1
+		  	A[i][3] = A[i][4] = A[i][5] = 0
+		  	A[i+4][0] = A[i+4][1] = A[i+4][2] = 0 
+		  	A[i][6] = (-src[i][0]) * (dst[i][0])
+		  	A[i][7] = (-src[i][1]) * (dst[i][0])
+		  	A[i+4][6] = (-src[i][0]) * (dst[i][1])
+		  	A[i+4][7] = (-src[i][1]) * (dst[i][1])
+		  	B[i] =(dst[i][0])
+		  	B[i+4] = dst[i][1]
+		cv2.solve(A,B,X)
+		M = Algoritmos.get_M(X,M) 
+		return M 
+
+	def RunGetPerspective(tl1, tr1, br1, bl1,img1):
+
+		img = cv2.imread(img1)
+		rows,cols,ch = img.shape
+
+		tl, tr, br, bl = tl1, tr1, br1, bl1
+		widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+		widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+		maxWidth = max(int(widthA), int(widthB))
+		heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+		heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+		maxHeight = max(int(heightA), int(heightB))
+		dst = np.array([
+			[0, 0],
+			[maxWidth , 0],
+			[0, maxHeight ],
+			[maxWidth , maxHeight ]])
+
+		pts1 = np.float32([tl,tr,br,bl])
+		pts2 = np.float32(dst)
+		M2 = Algoritmos.getPerspectiveTransform(pts1,pts2)
+		dst2 = Algoritmos.warPerspective(img,M2,(maxWidth,maxHeight))
+
+		salidaImg = "static/getPerspectiveTransform" + img1 
+		cv2.imwrite(salidaImg,dst2)
+		return salidaImg
+	def getNumber(strg):
+
+		num = []
+		str1 = ''
+				
+		for i in range(len(strg)):
+			if(strg[i].isdigit()):
+				str1 = str1 + strg[i]
+			elif(strg[i] == ','):
+				num.append(int(str1))
+				str1 = ''
+		num.append(int(str1))
+		return num
+
+
 class Operadores():
 	def inicio(request):
 		return render(request,'Home.html')
+
+
 		
 	def PageOperador(request):
 		tipo = request.POST['fase']
@@ -468,6 +548,8 @@ class Operadores():
 			return render(request,'OperadoresLogicos.html',{"labels":tipo})
 		elif(tipo == "XOR" ):
 			return render(request,'OperadoresLogicos.html',{"labels":tipo})
+		elif(tipo == "CamScanner"):
+			return render(request,'prueba.html',{"labels":tipo})
 
 	def ControladorOperador(request):
 
@@ -625,7 +707,25 @@ class Operadores():
 			resultado = Algoritmos.Ope_Logicos(file_name,file_name2,tipo)
 		
 			return render(request,'ResulTOperador.html',{"labels2":tipo,"image":"/"+resultado} )
+		if (tipo == "CamScanner"):
+			#return render(request,'Home.html')
 
+
+			xy1 = request.POST['xy1']
+			xy2 = request.POST['xy2']
+			xy3 = request.POST['xy3']
+			xy4 = request.POST['xy4']
+			print("xy1-- >", xy1)
+			print("xy2-- >", xy2)
+			print("xy3-- >", xy3)
+			print("xy4-- >", xy4)
+
+			#tl= Algoritmos.getNumber(xy1)
+			tl, tr, br, bl = Algoritmos.getNumber(xy1),Algoritmos.getNumber(xy4), Algoritmos.getNumber(xy2),Algoritmos.getNumber(xy3)
+			#src = [tl, tr, br, bl]
+			#print("PP ---> ", src)
+			resultado = Algoritmos.RunGetPerspective(tl, tr, br, bl,filename)
+			return render(request,'ResulTOperador.html',{"labels2":tipo,"image":"/"+resultado} )
 		return render(request,'Home.html')
 
 
